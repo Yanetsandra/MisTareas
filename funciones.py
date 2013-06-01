d507cf1f511e105fa03a584014860b979e376c73
Yanetsandra <sandrirob@gmail.com>
2013-05-31 20:21:09 -0600

    subiendo el archivo funciones

---
+++ funciones.py
@@ -0,0 +1,14 @@
+from lib.operaciones import *
+
+valor = int(raw_input('ingrese un valor'))
+valor2 = int(raw_input('ingrese otro valor'))
+operacion = raw_input('que operacion desea realizar : {s - r - m - d}')
+
+if operacion.lower() == 's':
+    print suma(valor,valor2)
+elif operacion.lower() == 'r':
+    print resta(valor,valor2)
+elif operacion.lower() == 'm':
+    print multiplicacion(valor,valor2)
+elif operacion.lower() == 'd':
+    print division(valor,valor2)
